152f03d9ab72d57c647a9f4a34c13da8ac291751
Nicolas de Almeida Mavroucakis <132313941+nicolasMavroucakis@users.noreply.github.com>
2026-05-26 19:58:19 -0300

    tirando o multi az

--- terraform/main.tf
+++ terraform/main.tf
@@ -327,7 +327,7 @@ resource "aws_db_instance" "mysql" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
-  multi_az               = true
+  multi_az               = false
   publicly_accessible    = false
   skip_final_snapshot    = true
 
